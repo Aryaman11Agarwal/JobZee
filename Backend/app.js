514f86de6936a2b1c78c17dd0b4afc0c7fe44f56
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-27 15:09:24 +0530

    routes added in app.js file

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const userRouter = require('./routes/userRouter.js');
+const applicationRouter = require('./routes/userRouter.js');
+const jobRouter = require('./routes/jobRouter.js');
 
 const app =express();
 dotenv.config({path:"./config/config.env"});
@@ -12,6 +15,8 @@ origin: [process.env.FRONTEND_URL],
 methods:['GET','PUT','POST','DELETE'],
 credentials:true
 }));
+
+
 
 
 app.use(
@@ -26,5 +31,10 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+
+app.use('/api/v1/user',userRouter);
+app.use('/api/v1/application',applicationRouter);
+app.use('/api/v1/job',jobRouter);
 
 module.exports=app;
