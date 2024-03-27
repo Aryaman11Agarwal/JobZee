ef79003fb0516651039042b4d627b6e3488dae05
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-27 11:59:50 +0530

    built in middlewares added

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
 
 const app =express();
 dotenv.config({path:"./config/config.env"});
+
+app.use(cors({
+origin: [process.env.FRONTEND_URL],
+methods:['GET','PUT','POST','DELETE'],
+credentials:true
+}));
+
+
+app.use(
+    fileUpload(
+        {
+            useTempFiles:true,
+            tempFileDir:"/tmp/",
+        }
+    )
+)
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 module.exports=app;
