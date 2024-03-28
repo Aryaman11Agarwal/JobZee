888ee1bb7ef91be0e6e02cda5ed71dd01699ba1a
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-28 18:22:44 +0530

    changed due to new line chars

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,19 +1,24 @@
 const catchAysncError = require('./catchAsyncError.js');
 const {ErrorHandler} = require('./error.js');
 const jwt = require('jsonwebtoken');
-const user = require('../models/userModel.js');
+const userModel = require('../models/userModel.js');
 
 
 const isAuthorised=catchAysncError(async (req,res,next)=>{
     const {token}=req.cookies;
-
+       console.log(token);
     if(!token){
         return next(new ErrorHandler("user not authorised",404));
     }
 
-    const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+    const decoded= jwt.verify(token,process.env.JWT_SECRET_KEY);
+    
 
-    req.user=await user.findById(decoded.id);
+    req.user=await userModel.findById(decoded.id);
+
+
+
+    
 
     next();
 
