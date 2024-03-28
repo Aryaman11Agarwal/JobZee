2b03a53e5067ba9733b1ac0d3089a3a953c198ea
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-28 18:22:04 +0530

    added post job functionality

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const {getAllJobs,postjob} = require('../controllers/jobController.js');
+const isAuthorised = require('../middlewares/auth.js');
+
+
+
 const router=express();
 
+
+router.get('/getall',getAllJobs)
+router.post('/post',isAuthorised,postjob)
 module.exports=router;
