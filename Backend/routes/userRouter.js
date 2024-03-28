656767fe7a81c7b50fbcc0beae2446dd56d82c5e
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-28 14:01:05 +0530

    added register login signup

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const isAuthorised = require('../middlewares/auth.js');
 
 const router=express();
+
+const  {register,login,logout}= require('../controllers/userController.js');
+
+router.post('/register',register);
+router.post('/login',login);
+router.get('/logout',isAuthorised,logout)
 
 module.exports=router;
