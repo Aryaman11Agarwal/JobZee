4bed203c430f027339bfef829b26592e15c1251a
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-29 20:59:30 +0530

    Backend Completed

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,21 +1,18 @@
-const catchAysncError = require('./catchAsyncError.js');
-const {ErrorHandler} = require('./error.js');
-const jwt = require('jsonwebtoken');
-const userModel = require('../models/userModel.js');
+const catchAysncError = require("./catchAsyncError.js");
+const { ErrorHandler } = require("./error.js");
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/userModel.js");
 
+const isAuthorised = catchAysncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  // console.log(token);
+  if (!token) {
+    return next(new ErrorHandler("user not authorised", 404));
+  }
 
-const isAuthorised=catchAysncError(async (req,res,next)=>{
-    const {token}=req.cookies;
-      // console.log(token);
-    if(!token){
-        return next(new ErrorHandler("user not authorised",404));
-    }
-
-    const decoded= jwt.verify(token,process.env.JWT_SECRET_KEY);
-    req.user=await userModel.findById(decoded.id);
-next();
-
-
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  req.user = await userModel.findById(decoded.id);
+  next();
 });
 
-module.exports=isAuthorised;
+module.exports = isAuthorised;
