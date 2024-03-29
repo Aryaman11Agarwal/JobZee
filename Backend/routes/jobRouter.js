fde056bfb7a890cf335a963aa04beeae7f2c193a
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-29 13:44:58 +0530

    added update and delete routes in job routes

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const {getAllJobs,postjob} = require('../controllers/jobController.js');
+const {getAllJobs,postjob,getmyJobs,updateJob,deleteJob} = require('../controllers/jobController.js');
 const isAuthorised = require('../middlewares/auth.js');
 
 
@@ -10,4 +10,8 @@ const router=express();
 
 router.get('/getall',getAllJobs)
 router.post('/post',isAuthorised,postjob)
+router.get('/getmyJobs',isAuthorised,getmyJobs);
+router.post('/updateJob/:id',isAuthorised,updateJob)
+router.get('/deleteJob/:id',isAuthorised,deleteJob)
+
 module.exports=router;
