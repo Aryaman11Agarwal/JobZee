885a8813ec20d6efe74429485693519ef1b96961
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-29 20:58:27 +0530

    Added getUser end points in job router

--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -3,10 +3,11 @@ const isAuthorised = require('../middlewares/auth.js');
 
 const router=express();
 
-const  {register,login,logout}= require('../controllers/userController.js');
+const  {register,login,logout,getUser}= require('../controllers/userController.js');
 
 router.post('/register',register);
 router.post('/login',login);
 router.get('/logout',isAuthorised,logout)
+router.get('/getUser',isAuthorised,getUser);
 
 module.exports=router;
