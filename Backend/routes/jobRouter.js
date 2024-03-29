e3e4a9907affb543fe1adc8e9c40765767a90ae1
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-29 20:57:59 +0530

    Added update and delete end points in job router

--- Backend/routes/jobRouter.js
+++ Backend/routes/jobRouter.js
@@ -11,7 +11,7 @@ const router=express();
 router.get('/getall',getAllJobs)
 router.post('/post',isAuthorised,postjob)
 router.get('/getmyJobs',isAuthorised,getmyJobs);
-router.post('/updateJob/:id',isAuthorised,updateJob)
-router.get('/deleteJob/:id',isAuthorised,deleteJob)
+router.put('/updateJob/:id',isAuthorised,updateJob)
+router.delete('/deleteJob/:id',isAuthorised,deleteJob)
 
 module.exports=router;
