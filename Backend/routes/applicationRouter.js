afa05ccb4b804aacb40ba7ddd83d84a996567975
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-03-29 20:56:30 +0530

    completed application router

--- Backend/routes/applicationRouter.js
+++ Backend/routes/applicationRouter.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const {employerGetAllAplications,jobseekerDeleteApplication,jobseekerGetAllAplications,postApplication} = require('../controllers/applicationController.js');
+const isAuthorised = require('../middlewares/auth.js');
+
 const router=express();
+
+router.get('/employer/getall',isAuthorised,employerGetAllAplications),
+router.get('/jobseeker/getall',isAuthorised,jobseekerGetAllAplications)
+router.delete('/delete/:id',isAuthorised,jobseekerDeleteApplication),
+router.post('/post',isAuthorised,postApplication)
 
 module.exports=router;
