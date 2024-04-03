5e99c1292cfbea4b68c8003cd99b6054c07e3354
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-04 00:17:24 +0530

    ToastContainer added

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export const Context=createContext();
+
+const AppWrapper=()=>{
+  const [isAuthorised,setIsAuthorised]=useState(false);
+  const [user,setUser]=useState({});
+
+  return (
+    <Context.Provider value={{isAuthorised,setIsAuthorised,user,setUser}}>
+       <App></App>
+       
+    </Context.Provider>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AppWrapper />
+    <ToastContainer />
+  </React.StrictMode>
 )
