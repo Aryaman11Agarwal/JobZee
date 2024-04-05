f44cafb88e5b97d1ed38908fdf7bdea338ed2959
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-05 13:40:11 +0530

    Home completed

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../../main'
+import { Navigate } from 'react-router-dom';
+import HeroSection from './HeroSection';
+import HowItWorks from './HowItWorks'
+import PopularCategories from './PopularCategories'
+import PopularCompanies from './PopularCompanies'
+
 
 const Home = () => {
+  const {isAuthorised}=useContext(Context);
+
+  if(!isAuthorised){
+    return <Navigate to={"/login"}></Navigate>
+  }
   return (
-    <div>
-      
-    </div>
+    <section className='homePage page'>
+    <HeroSection></HeroSection>
+    <HowItWorks></HowItWorks>
+    <PopularCategories></PopularCategories>
+    <PopularCompanies></PopularCompanies>
+
+    </section>
   )
 }
 
