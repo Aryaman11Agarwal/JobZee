1d6b4aa51a6cf20df2b331344033c88ab16cc6ef
Aryaman11Agarwal <aryaman1110@gmail.com>
2024-04-06 15:04:58 +0530

    Get job Details frontend working

--- Frontend/src/components/Job/JobDetails.jsx
+++ Frontend/src/components/Job/JobDetails.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useContext, useState,useEffect } from 'react'
+import {useNavigate,useParams,Link} from 'react-router-dom'
+import { Context } from '../../main';
+import { IoEllipseSharp } from 'react-icons/io5';
 
 const JobDetails = () => {
+  const {id}=useParams()
+  const [job,setJob]=useState({});
+  const navigateTo=useNavigate();
+  const {isAuthorised,user}=useContext(Context);
+
+  useEffect(()=>{
+    fetch(`http://localhost:8000/api/v1/job/${id}`,{
+      method:"GET",
+      credentials:"include"
+    })
+    .then(response=>response.json())
+    .then((data)=>{
+      if(data.success===true){
+        setJob(data.job);
+      }
+      else{
+
+      }
+    })
+    .catch((e)=>console.log(`in error ${e}`));
+  },[])
+
+  if(!isAuthorised){
+    navigateTo('/login');
+  }
+
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="jobDetail page">
+      <div className="container">
+        <h3>Job Details</h3>
+        <div className="banner">
+          <p>
+            Title: <span> {job.title}</span>
+          </p>
+          <p>
+            Category: <span>{job.category}</span>
+          </p>
+          <p>
+            Country: <span>{job.country}</span>
+          </p>
+          <p>
+            City: <span>{job.city}</span>
+          </p>
+          <p>
+            Location: <span>{job.location}</span>
+          </p>
+          <p>
+            Description: <span>{job.description}</span>
+          </p>
+          <p>
+            Job Posted On: <span>{job.jobPostedOn}</span>
+          </p>
+          <p>
+            Salary:{" "}
+            {job.fixedSalary ? (
+              <span>{job.fixedSalary}</span>
+            ) : (
+              <span>
+                {job.salaryFrom} - {job.salaryTo}
+              </span>
+            )}
+          </p>
+          {user && user.role === "Employer" ? (
+            <></>
+          ) : (
+            <Link to={`/application/${job._id}`}>Apply Now</Link>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default JobDetails
